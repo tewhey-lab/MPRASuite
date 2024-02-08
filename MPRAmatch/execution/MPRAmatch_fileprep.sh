ed79d63b6d29a6e56c67d4493115a1b477887c2f
HarshpreetChandok <46939896+HarshpreetChandok@users.noreply.github.com>
2024-02-08 11:20:02 -0600

    Adding MPRAmatch

---
+++ MPRAmatch/execution/MPRAmatch_fileprep.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 40
+#SBATCH --time=72:00:00
+#SBATCH --mem 200G
+#SBATCH --mail-user=harshpreet.chandok@jax.org	
+#SBATCH --mail-type=BEGIN,END,FAIL	
+#SBATCH --output=MPRAmatch-%x.%j.out
+#SBATCH --error=MPRAmatch-%x.%j.err	
+
+out=$1
+proj=$2
+config_file=$3
+
+source ${config_file}
+
+cmd=${pwd}
+outdir=${out}/outputs/MPRAmatch
+fastq_loc=${out}/inputs
+log_file="${out}/${now}_${proj}_MPRAmatch_log.txt"
+
+module load singularity
+
+
+#*******************Step 1: Merge delta GFP Fastq files*******************
+
+for i in `awk '{print $2}' ${acc_file} | sort | uniq`
+do
+echo $i
+CMD=`awk -v i="$i" '$2==i {aggr=aggr $1" "} END {print aggr}' ${acc_file}`
+echo $CMD
+cat $CMD > ${fastq_loc}/$i.fastq.gz
+done
+
+
+#********Step 2: Rename Fasta reference file with library name ************
+
+cp ${fasta} ${out}/inputs
+fasta_name=$(basename ${fasta})
+mv ${out}/inputs/${fasta_name} ${out}/inputs/${proj}_reference.fasta
+gzip ${out}/inputs/${proj}_reference.fasta 
+
+for file in ${out}/inputs/*gz
+do
+	echo "Checking status for file: '$file'"
+if [[ ! -f $file && ! -s $file ]];then
+	echo "$file not found or empty"
+  	exit 1
+fi
+done
+
+
+#*******************Step 3: Fill in the match json file**********************
+
+
+cp ${gitrepo_dir}/MPRAmatch/setup/MPRAmatch_input.json ${out}/MPRAmatch_${proj}_inputs.json
+
+singularity exec ${jq_container} jq --arg proj ${proj} --arg FLOC ${out}/inputs --arg OUT ${proj} -M '. + {"MPRAmatch.read_a":'\"${out}/inputs/${proj}_r1.fastq.gz\"', "MPRAmatch.read_b":'\"${out}/inputs/${proj}_r2.fastq.gz\"', "MPRAmatch.reference_fasta":'\"${out}/inputs/${proj}_reference.fasta.gz\"', "MPRAmatch.id_out":'\"${proj}\"',"MPRAmatch.out_directory":'\"${out}/outputs/MPRAmatch\"'}' ${out}/MPRAmatch_${proj}_inputs.json > ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_inputs.json
+
+
+if [[ ! -f  ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_inputs.json  && ! -s  ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_inputs.json ]];then
+        echo "ERROR:MPRAmatch JSON file not found"
+        exit 1
+fi
+
+
+#*******************Step 4: Create the MPRAmatch_call script*******************
+
+
+echo -e " echo 'Running Cromwell'" > ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_call.sh
+
+echo -e "cromwell run ${gitrepo_dir}/MPRAmatch/MPRAmatch.wdl --inputs ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_inputs.json" >> ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_call.sh
+
+echo -e "echo 'Finished Cromwell'" >> ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_call.sh
+
+
+
+#*******************Step 5: Execute MPRAmatch_call WDL analysis pipeline*********
+
+
+cd ${out}/execution/${now}_${proj}_MPRAmatch/
+
+echo "Loading Singularity Module"
+
+echo "Executing SIF with Code"
+
+singularity exec ${mpra_oligo_container} sh ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_call.sh
+
+echo "Done"
+
+
+#***************Step 6: Copy WDL and Slurm log files***************************
+
+cp ${results_dir}/MPRAmatch-${proj}.${SLURM_JOB_ID}.out ${out}/slurm_logs
+rm ${out}/MPRAmatch_${proj}_inputs.json
+mv ${out}/slurm_logs/.out ${out}/slurm_logs/${now}_${proj}_MPRAmatch_cromwell-workflow-logs
+#mv ${cmd}/cromwell-workflow-logs ${out}/execution/${now}_${proj}_MPRAmatch
+
+
+#**************Step 7: Save status and location of output files*****************
+
+echo "Results for library analyzed ${proj} are located in directory ${out}" > ${log_file}
+echo "The concatenated delta GFP fastq files are located at: ${fastq_loc}" >> ${log_file}
+echo "The reference fasta file is located at: ${out}/inputs/${proj}_reference.fasta.gz" >> ${log_file}
+echo "The JSON file with MPRAmatch input parameters is located at: ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_inputs.json" >> ${log_file}
+echo "The script to run the MPRAmatch WDL pipeline is located at: ${out}/execution/${now}_${proj}_MPRAmatch/MPRAmatch_${proj}_call.sh" >> ${log_file}
+echo "SLURM Job ID: ${SLURM_JOB_ID}" >> ${log_file}
+
---
+++ MPRAmatch/execution/MPRAmatch_run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name=MPRAmatch_run_"${proj}"
+#SBATCH -N 1
+#SBATCH -n 70
+#SBATCH --time=72:00:00
+#SBATCH --mem 350G
+#SBATCH --mail-user=harshpreet.chandok@jax.org	
+#SBATCH --output=MPRAmatch-%x.%j.out
+#SBATCH --error=MPRAmatch-%x.%j.err	
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+config_file=$1
+source ${config_file}
+
+now=$(date +"%y%m%d-%H%M%S")
+mkdir -p ${results_dir}/${now}_${library_rerun_name} 
+out=${results_dir}/${now}_${library_rerun_name}
+
+
+##Setting up the output folders
+
+mkdir -p ${out}/outputs/MPRAmatch
+mkdir -p ${out}/execution/
+mkdir -p ${out}/execution/${now}_${proj}_MPRAmatch
+mkdir -p ${out}/inputs
+mkdir -p ${out}/slurm_logs
+
+
+slurm_logfile="${out}/slurm_logs/${SLURM_ARRAY_TASK_ID}.out"
+
+echo "Writing to ${slurm_logfile}"
+scontrol show -dd job $SLURM_JOB_ID
+printenv
+
+
+echo ${out}
+echo ${proj}
+
+source ${gitrepo_dir}/MPRAmatch/execution/MPRAmatch_fileprep.sh ${out} ${proj} ${config_file} ${SLURM_ARRAY_TASK_ID} > ${slurm_logfile}
